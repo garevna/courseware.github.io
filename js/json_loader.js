3b0ac34b4b2ead3f7f90836f4bded964fc9e8a76
garevna <garevna@mail.ru>
2017-09-19 19:04:19 +0300

    Update json_loader.js

--- js/json_loader.js
+++ js/json_loader.js
@@ -8,7 +8,7 @@ function getTemplates () {
 	var templatesRequest = transport ();
 	templatesRequest.onreadystatechange = function () {
 		if ( templatesRequest.readyState == 4 ) {
-			if ( templatesRequest.status == 200 )  templates = JSON.parse ( templatesRequest.responseText );
+			if ( templatesRequest.status == 200 )  self.templates = JSON.parse ( templatesRequest.responseText );
 			else console.warn ( 'Templates is not avaliable. Request status: ' + templatesRequest.status );
 		}
 	}
@@ -25,10 +25,10 @@ this.addEventListener( 'message', function ( e ) {
 		if ( $request.readyState == 4 ) {
 			if ( $request.status == 200 ) {
 				var answer = JSON.parse ( $request.responseText);
-				if ( perspective ) {
+				if ( perspective && self.templates ) {
 					for ( var i = 0; i < answer.levels; i++ ) {
 						if ( answer.levels [i].template ) {
-							answer.levels [i] = templates [ answer.levels [i].template ];
+							answer.levels [i] = self.templates [ answer.levels [i].template ];
 						}
 					}
 				}
