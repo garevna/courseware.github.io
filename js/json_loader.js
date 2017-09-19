25c6a3b3d8637ee7a3393e343b3293208ceabd76
garevna <garevna@mail.ru>
2017-09-19 18:15:52 +0300

    add loading templates file

--- js/json_loader.js
+++ js/json_loader.js
@@ -3,7 +3,17 @@ function transport () {
 	else var requestVar = new ActiveXObject ( "Microsoft.XMLHTTP" );
 	return requestVar;
 };
-
+var templates = [];
+var templatesRequest = transport ();
+templatesRequest.onreadystatechange = function () {
+	if ( $request.readyState == 4 ) {
+		if ( $request.status == 200 ) {
+			templates = JSON.parse ( $request.responseText );
+			console.log ( templates );
+		}
+		else { console.warn ( 'Templates is not avaliable. Request status: ' + $request.status ); postMessage(null); }
+	}
+}
 this.addEventListener( 'message', function ( e ) {
 	var sourceURL = e.data;
 	var $request = transport ();
