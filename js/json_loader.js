eb91ce98d56a1e80356cb7bdede3099b9802cf41
garevna <garevna@mail.ru>
2017-09-19 18:54:07 +0300

    Update json_loader.js

--- js/json_loader.js
+++ js/json_loader.js
@@ -3,34 +3,34 @@ function transport () {
 	else var requestVar = new ActiveXObject ( "Microsoft.XMLHTTP" );
 	return requestVar;
 };
-templates = [];
-var templatesRequest = transport ();
-templatesRequest.onreadystatechange = function () {
-	if ( templatesRequest.readyState == 4 ) {
-		if ( templatesRequest.status == 200 ) {
-			templates = JSON.parse ( templatesRequest.responseText );
-			console.dir ( templates );
+
+if ( !templates ) {
+	var templatesRequest = transport ();
+	templatesRequest.onreadystatechange = function () {
+		if ( templatesRequest.readyState == 4 ) {
+			if ( templatesRequest.status == 200 )  templates = JSON.parse ( templatesRequest.responseText );
+			else console.warn ( 'Templates is not avaliable. Request status: ' + templatesRequest.status );
 		}
-		else { console.warn ( 'Templates is not avaliable. Request status: ' + templatesRequest.status ); postMessage(null); }
 	}
+	templatesRequest.open ( "GET", "json/templates.json" );
+	templatesRequest.send ();
 }
-templatesRequest.open ( "GET", "json/templates.json" );
-templatesRequest.send ();
 
 this.addEventListener( 'message', function ( e ) {
 	var sourceURL = e.data;
+	var perspective = sourceURL.indexOf ( 'perspective' ) >= 0;
 	var $request = transport ();
 	$request.onreadystatechange = function () {
 		if ( $request.readyState == 4 ) {
 			if ( $request.status == 200 ) {
 				var answer = JSON.parse ( $request.responseText);
-				for ( var i = 0; i < answer.levels; i++ ) {
-					if ( answer.levels [i].template ) {
-						answer.levels [i] = templates [ answer.levels [i].template ];
-						console.log ( answer.levels [i].template, answer.levels [i] );
+				if ( perspective ) {
+					for ( var i = 0; i < answer.levels; i++ ) {
+						if ( answer.levels [i].template ) {
+							answer.levels [i] = templates [ answer.levels [i].template ];
+						}
 					}
 				}
-				
 				postMessage ( answer );
 			}
 			else { console.warn ( 'request.status: ' + $request.status ); postMessage(null); }
