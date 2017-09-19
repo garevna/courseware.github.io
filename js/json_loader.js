342a462adb36c8f27cd325018c3f0de3a9534a6d
garevna <garevna@mail.ru>
2017-09-19 19:42:12 +0300

    Update json_loader.js

--- js/json_loader.js
+++ js/json_loader.js
@@ -8,12 +8,22 @@ function getTemplates () {
 	var templatesRequest = transport ();
 	templatesRequest.onreadystatechange = function () {
 		if ( templatesRequest.readyState == 4 ) {
-			if ( templatesRequest.status == 200 )  self.templates = JSON.parse ( templatesRequest.responseText );
-			else console.warn ( 'Templates is not avaliable. Request status: ' + templatesRequest.status );
+			if ( templatesRequest.status == 200 )  {
+				self.templates = JSON.parse ( templatesRequest.responseText );
+				setAnswer ();
+			} else console.warn ( 'Templates is not avaliable. Request status: ' + templatesRequest.status );
 		}
 	}
 	templatesRequest.open ( "GET", "../json/templates.json" );
 	templatesRequest.send ();
+}
+function setAnswer () {
+	if ( !self.templates || !self.answer ) return;
+	console.info ( 'answer is ready' );
+	for ( var i = 0; i < answer.levels; i++ ) {
+		if ( self.answer.levels [i].template ) self.answer.levels [i] = self.templates [ self.answer.levels [i].template ];
+	}
+	postMessage ( self.answer );
 }
 
 this.addEventListener( 'message', function ( e ) {
@@ -24,16 +34,8 @@ this.addEventListener( 'message', function ( e ) {
 	$request.onreadystatechange = function () {
 		if ( $request.readyState == 4 ) {
 			if ( $request.status == 200 ) {
-				var answer = JSON.parse ( $request.responseText);
-				if ( perspective && self.templates ) {
-					console.log ('templates: ', self.templates);
-					for ( var i = 0; i < answer.levels; i++ ) {
-						if ( answer.levels [i].template ) {
-							answer.levels [i] = self.templates [ answer.levels [i].template ];
-						}
-					}
-				}
-				postMessage ( answer );
+				self.answer = JSON.parse ( $request.responseText);
+				setAnswer();
 			}
 			else { console.warn ( 'request.status: ' + $request.status ); postMessage(null); }
 		}
