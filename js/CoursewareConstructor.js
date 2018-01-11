1b71164920cac76d12824b414d13cde47abb314c
garevna <garevna@mail.ru>
2018-01-11 17:03:12 +0200

    Update CoursewareConstructor.js

--- js/CoursewareConstructor.js
+++ js/CoursewareConstructor.js
@@ -284,8 +284,8 @@ function CoursewareConstructor () {
 	// ====================================================================================== resizeChoiceLevel
 	function resizeChoiceLevel () {
 		var center = { top: Math.round( ( window.innerHeight - 80 )/2), 
-			      left: Math.round( window.innerWidth * 0.8 /2) }
-		var radius = Math.round(Math.min ( window.innerHeight, window.innerWidth ) * 0.3)
+			      left: Math.round( window.innerWidth * 0.9 /2) }
+		var radius = Math.round(Math.min ( window.innerHeight, window.innerWidth * 0.9 ) * 0.3)
 		var delta = Math.round(radius/Math.sqrt(2))
 		var points = [
 			{ top: center.top - radius, left: center.left },
